c60e52e109148391651bd3e4fd6cdded910a8b01
mag1cianag <mag1cian@aliyun.com>
2021-10-27 16:30:12 +0800

    q1 finish

--- proj1.sql
+++ proj1.sql
@@ -19,31 +19,44 @@ DROP VIEW IF EXISTS q4v;
 -- Question 0
 CREATE VIEW q0(era)
 AS
-  SELECT 1 -- replace this line
+  SELECT Max(era)
+  FROM pitching
 ;
 
 -- Question 1i
 CREATE VIEW q1i(namefirst, namelast, birthyear)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT namefirst, namelast, birthyear 
+  FROM people
+  WHERE weight > 300
 ;
 
 -- Question 1ii
 CREATE VIEW q1ii(namefirst, namelast, birthyear)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT namefirst, namelast ,birthyear
+  FROM people
+  WHERE namefirst LIKE '% %'
+  ORDER BY namefirst , namelast
 ;
 
 -- Question 1iii
 CREATE VIEW q1iii(birthyear, avgheight, count)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT birthyear,AVG(height),COUNT(*)
+  FROM people
+  GROUP BY birthyear
+  ORDER BY birthyear
 ;
 
 -- Question 1iv
 CREATE VIEW q1iv(birthyear, avgheight, count)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT birthyear,AVG(height) as a,COUNT(*)
+  FROM people
+  GROUP BY birthyear
+  HAVING a > 70
+  ORDER BY birthyear
 ;
 
 -- Question 2i
